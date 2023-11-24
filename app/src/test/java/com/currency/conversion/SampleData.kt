983edd30e13665d1cacf5bac865e5c1a04f05832
LiangYuanRui <liangyuanrui313@163.com>
2023-11-24 17:07:15 +0800

    add ExchangeRateRemoteDataSourceTest

---
+++ app/src/test/java/com/currency/conversion/SampleData.kt
@@ -0,0 +1,16 @@
+package com.currency.conversion
+
+import com.exchangerate.converter.data.remote.model.ExchangeRateResponse
+
+object SampleData {
+
+    val exchangeRateResponse = ExchangeRateResponse(
+        "USD", mapOf(
+            "AED" to 3.672475,
+            "JPY" to 69.085468,
+            "CNY" to 7.8,
+            "ERU" to 322.0
+        )
+    )
+
+}
--- app/src/test/java/com/currency/conversion/data/remote/ExchangeRateRemoteDataSourceTest.kt
+++ app/src/test/java/com/currency/conversion/data/remote/ExchangeRateRemoteDataSourceTest.kt
@@ -1,16 +1,23 @@
 package com.currency.conversion.data.remote
 
-import com.exchangerate.converter.data.remote.api.ExchangeRateApiService
-import com.exchangerate.converter.data.model.ExchangeRate
+import com.currency.conversion.SampleData
 import com.exchangerate.converter.data.remote.ExchangeRateRemoteDataSource
+import com.exchangerate.converter.data.remote.api.ExchangeRateApiService
+import com.exchangerate.converter.data.remote.model.ExchangeRateResponse
+import com.exchangerate.converter.util.Constants
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
 import retrofit2.Response
 
+@ExperimentalCoroutinesApi
 @RunWith(MockitoJUnitRunner::class)
 class ExchangeRateRemoteDataSourceTest {
 
@@ -25,11 +32,27 @@ class ExchangeRateRemoteDataSourceTest {
     }
 
     @Test
-    fun testGetExchangeRate() = runTest {
-        val fakeExchangeRate = ExchangeRate(mapOf("USD" to 1.0, "EUR" to 0.85))
-        val fakeResponse = Response.success(fakeExchangeRate)
-//        `when`(exchangeRateApiService.getLatestRate(anyString())).thenReturn(fakeResponse)
-//        val result = exchangeRateRemoteDataSource.getExchangeRate()
-//        assertEquals(fakeResponse, result)
+    fun `getExchangeRate returns successful response`() = runTest {
+        val mockResponse = Response.success(SampleData.exchangeRateResponse)
+        `when`(exchangeRateApiService.getLatestRate(Constants.OPEN_EXCHANGE_API_ID)).thenReturn(
+            mockResponse
+        )
+
+        val response = exchangeRateRemoteDataSource.getExchangeRate().first()
+
+        assert(response.isSuccessful && response.body() != null)
+    }
+
+    @Test
+    fun `getExchangeRate handles API failure`() = runTest {
+        val errorResponse =
+            Response.error<ExchangeRateResponse>(404, "".toResponseBody(null))
+        `when`(exchangeRateApiService.getLatestRate(Constants.OPEN_EXCHANGE_API_ID)).thenReturn(
+            errorResponse
+        )
+
+        val response = exchangeRateRemoteDataSource.getExchangeRate().first()
+
+        assert(!response.isSuccessful)
     }
 }
