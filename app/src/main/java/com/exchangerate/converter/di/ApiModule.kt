ed4a2021b9ba39705a5692a973b663680fb6e992
LiangYuanRui <liangyuanrui313@163.com>
2023-11-30 22:48:31 +0800

    refactor di module

--- app/src/main/java/com/exchangerate/converter/di/ApiModule.kt
+++ app/src/main/java/com/exchangerate/converter/di/ApiModule.kt
@@ -1,42 +1,16 @@
 package com.exchangerate.converter.di
 
 import com.exchangerate.converter.data.remote.api.ExchangeRateApiService
-import com.exchangerate.converter.util.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Converter
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ApiModule {
-
-    @Singleton
-    @Provides
-    fun provideHttpLoggingInterceptor() = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
-
-    @Singleton
-    @Provides
-    fun provideOkhttp(interceptor: HttpLoggingInterceptor) = OkHttpClient.Builder().addInterceptor(interceptor).build()
-
-    @Singleton
-    @Provides
-    fun provideConverterFactory() : Converter.Factory = MoshiConverterFactory.create()
-
-    @Singleton
-    @Provides
-    fun provideRetrofit(okHttpClient: OkHttpClient, converterFactory: Converter.Factory): Retrofit = Retrofit.Builder()
-        .addConverterFactory(converterFactory)
-        .client(okHttpClient)
-        .baseUrl(Constants.OPENEXCHANGERATES_BASE_URL)
-        .build()
 
     @Singleton
     @Provides
